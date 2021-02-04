3ef892a59c45fa9b3d9caba564f2f8758c78582d
MartinHoffmannJena <43675767+MartinHoffmannJena@users.noreply.github.com>
2021-02-04 13:16:27 +0100

    minors

--- evaluation/tanimoto.py
+++ evaluation/tanimoto.py
@@ -1,5 +1,5 @@
 from rdkit import DataStructs, Chem
-from rdkit.Chem import MACCSkeys
+from rdkit.Chem import MACCSkeys, Draw
 import molecule_comparison
 
 inp="./training_smiles.txt"
@@ -21,3 +21,5 @@ for i in range(0,len(fps)-1):
 
     if mces[1]<=3 and tanimoto<0.7:    # Print out interesting pairs, for example "good" MCES and "bad" tanimoto
         print(str(mces[1])+" "+str(tanimoto)+" "+smiles[i]+" "+smiles[i+1])
+       # im = Chem.Draw.MolsToGridImage( [structs[i],structs[i+1]]) # This draws the molecule pair, without this you can parallize
+        #im.show()
