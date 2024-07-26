cb3e4a97f25722e1ffa64d2c0f0cd048345776bc
Fleming Kretschmer <fleming.kretschmer@uni-jena.de>
2024-07-26 11:40:20 +0200

    package restructured; README

--- src/myopic_mces/__init__.py
+++ src/myopic_mces/__init__.py
@@ -1,4 +1,6 @@
-# from .myopic_mces import MCES
-# from .MCES_ILP import MCES_ILP
-# from .filter_MCES import apply_filter, filter1, filter2
-# from .graph import construct_graph
+from .myopic_mces import MCES
+from .MCES_ILP import MCES_ILP
+from .filter_MCES import apply_filter, filter1, filter2
+from .graph import construct_graph
+
+__all__ = ['MCES', 'MCES_ILP', 'apply_filter', 'filter1', 'filter2', 'construct_graph']
