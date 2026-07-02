bf35269a5f465c302dc6d11311dcfefdb13f6845
Fleming Kretschmer <fleming.kretschmer@uni-jena.de>
2026-07-02 16:57:26 +0200

    combine hdf5 batches now with all important information

--- src/myopic_mces/combine_hdf5_batches.py
+++ src/myopic_mces/combine_hdf5_batches.py
@@ -22,6 +22,7 @@ if __name__ == '__main__':
 
     with h5py.File(args.input_batches[0], 'r') as f:
         smiles = list(f['smiles'])
+        computation_args = {k: f['computation_args'][k][()] for k in f['computation_args']}
         ninstances = int(binom(len(smiles), 2))
 
     if (args.two_datasets_shape is not None):
@@ -31,6 +32,7 @@ if __name__ == '__main__':
 
     all_indices = np.empty((ninstances, 3), dtype='int64')
     all_mces = np.empty((ninstances,), dtype='float32')
+    all_modes = np.empty((ninstances,), dtype='uint8')
         
     i = 0
     for batch in args.input_batches:
@@ -38,24 +40,36 @@ if __name__ == '__main__':
         with h5py.File(batch, 'r') as f:
             n = len(f['computation_indices'])
             f['mces'].read_direct(all_mces[i:(i+n)])
+            f['computation_modes'].read_direct(all_modes[i:(i+n)])
             f['computation_indices'].read_direct(all_indices[i:(i+n)])
             i += n
 
     print('restoring correct order')
     order = all_indices[:, 0].argsort()
     all_mces_order = all_mces[order]
+    all_modes_order = all_modes[order]
     all_indices_order = all_indices[order]
 
     if (args.two_datasets_shape is not None):
         print(f'reshaping computation matrix from {all_mces_order.shape} to {tuple(args.two_datasets_shape)}')
         all_mces_order = all_mces_order.reshape(tuple(args.two_datasets_shape))
+        all_modes_order = all_modes_order.reshape(tuple(args.two_datasets_shape))
         
 
     # TODO: sanity check, repeat and test some computations
         
     print('writing output to', args.out)
     with h5py.File(args.out, 'w') as f:
+        g = f.create_group('computation_args')
+        for k, v in computation_args.items():
+            g.create_dataset(k, data=v)
         f.create_dataset('mces', data=all_mces_order)
-        f.create_dataset('mces_smiles_order', data=smiles)
+        f.create_dataset('computation_modes_order', data=all_modes_order)
+        if (args.two_datasets_shape is not None):
+            n1, n2 = args.two_datasets_shape
+            f.create_dataset('smiles_dim1', data=smiles[n2:n1+n2])
+            f.create_dataset('smiles_dim2', data=smiles[:n2])
+        else:
+            f.create_dataset('mces_smiles_order', data=smiles)
 
     print(f'combined {len(all_indices_order)} MCES computations in {(time() - t0) / 60:.1f}min')
